a5459bb2655b09f9b07a30343f615f3becd0b0ea
Sandro Guillen <sdguillen@espe.edu.ec>
2021-11-02 21:26:25 -0400

    1 Ejercicio de Netbeans

---
+++ src/main/java/com/mycompany/holamundo/NewMain.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.mycompany.holamundo;
+
+/**
+ *
+ * @author sandr
+ */
+public class NewMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         // TODO code application logic here
+        System.out.print("Hola mundo "); /* Codigo para escribir hola mundo cuando se ejecuta */ 
+        System.out.print("Hola mundo 1"); /* Codigo para escribir hola mundo cuando se ejecuta */ 
+        System.out.print("Hola mundo 2"); /* Codigo para escribir hola mundo cuando se ejecuta */ 
+        System.out.print("Hola mundo 3"); /* Codigo para escribir hola mundo cuando se ejecuta */ 
+        System.out.print("Hola mundo 4"); /* Codigo para escribir hola mundo cuando se ejecuta */ 
+    }
+    
+}
